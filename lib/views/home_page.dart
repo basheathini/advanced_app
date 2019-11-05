82fa52da7a5e374b12f1412e324adfd292fb2a9a
masterbashe <bashe.athini@gmail.com>
2019-11-05 18:46:13 +0200

    Initial commit

---
+++ lib/views/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _batteryLevel;
+
+  Future<void> _getBatteryLevel() async {
+    const platform = MethodChannel('course.flutter.dev/battery');
+    try{
+      final batteryLevel = await platform.invokeMethod('getBatteryLevel');
+      setState(() {
+        _batteryLevel = batteryLevel;
+      });
+    }on PlatformException catch(error) {
+      setState(() {
+        _batteryLevel = null;
+      });
+
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getBatteryLevel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Native Code'),
+        actions: <Widget>[
+          Icon(Icons.battery_std)
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text('Battery levels : $_batteryLevel.', textAlign: TextAlign.center,)
+        ],
+
+      ),
+    );
+  }
+}
